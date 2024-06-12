46af7da119edf8110bb298b2ab4afdb68afcf6c6
cold <kholodihor@gmail.com>
2024-06-12 13:12:52 +0300

    feat: add ripple card

--- src/components/main/header/Header.tsx
+++ src/components/main/header/Header.tsx
@@ -2,7 +2,7 @@ import LanguageSwitcher from "./LanguageSwitcher"
 
 const Header = () => {
   return (
-    <header className="bg-graphite 3xl:px-[80px] flex h-[64px] w-full items-center justify-between px-4  py-[17px] xl:px-[40px]">
+    <header className="flex h-[64px] w-full items-center justify-between bg-graphite px-4 py-[17px]  xl:px-[40px] 3xl:px-[80px]">
       <img src="/images/header/logo.png" alt="ML" />
       <div className="flex gap-10 xl:gap-[80px]">
         <LanguageSwitcher />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next"
-import { Button } from "@/components/ui/button"
 import Header from "@/components/main/header/Header"
+import RippleCard from "../components/RippleCard"
 
 const HomePage = () => {
   const { t } = useTranslation()
@@ -8,10 +8,10 @@ const HomePage = () => {
     <>
       <Header />
       <main className="flex min-h-[70vh] w-full flex-col items-center justify-center gap-2 bg-black">
-        <h1 className="text-violet text-3xl text-[3rem] font-light">
+        <h1 className="text-3xl text-[3rem] font-light text-violet">
           {t("hello")}
         </h1>
-        <Button>Click me</Button>
+        <RippleCard />
       </main>
     </>
   )
