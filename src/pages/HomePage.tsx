911ff230bb35812da08028c8a6f56358cd9bd38b
cold <kholodihor@gmail.com>
2024-06-12 09:01:21 +0300

    feat(setup): add colors to tailwind.config

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,17 @@
+import Header from "@/components/main/Header"
 import { Button } from "@/components/ui/button"
 import { useTranslation } from "react-i18next"
 
 const HomePage = () => {
   const { t } = useTranslation()
   return (
-    <div className="flex min-h-[100vh] w-full flex-col items-center justify-center gap-2">
-      <h1 className="text-3xl text-green-500">{t("hello")}</h1>
-      <Button>Click me</Button>
-    </div>
+    <>
+      <Header />
+      <main className="bg-lime flex min-h-[70vh] w-full flex-col items-center justify-center gap-2">
+        <h1 className="text-3xl text-green-500">{t("hello")}</h1>
+        <Button>Click me</Button>
+      </main>
+    </>
   )
 }
 
