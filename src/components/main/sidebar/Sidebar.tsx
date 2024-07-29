f12b703eee1cba6c3d8de7c3ddffd714d5070761
SvitlanaFediai <svitlana160393@gmail.com>
2024-07-29 19:54:07 +0300

    useLocation

--- src/components/main/sidebar/Sidebar.tsx
+++ src/components/main/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import CoursesIcon from "@/components/icons/dashboard/CourseIcon"
 import CalendarIcon from "@/components/icons/dashboard/CalendarIcon"
 import EnglishIcon from "@/components/icons/dashboard/SpeakingIcon"
