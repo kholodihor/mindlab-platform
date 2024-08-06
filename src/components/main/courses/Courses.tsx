42a51de84e828ca7dd7e4d057e1fff736dbc4839
Inna Shkilna <shkinai.1995@gmail.com>
2024-08-06 11:58:00 +0300

    Add translate to  cards

--- src/components/main/courses/Courses.tsx
+++ src/components/main/courses/Courses.tsx
@@ -5,6 +5,8 @@ import data from "@/data/courses/data.json"
 import SaleCards from "./SaleCards"
 import Cards from "./Cards"
 import Open from "@/components/icons/Open"
+import { CardSale } from "@/types/courses"
+import { CardData } from "../../../types/courses/index"
 
 const Courses = () => {
   const [showMore, setShowMore] = useState(false)
@@ -24,11 +26,25 @@ const Courses = () => {
   }
 
   const combinedData = [
-    ...coursesSale.map((course) => ({ ...course, type: "sale" })),
-    ...data.map((course) => ({ ...course, type: "regular" }))
+    ...coursesSale.map((course) => ({
+      ...course,
+      type: "sale"
+    })),
+    ...data.map((course) => ({
+      ...course,
+      type: "regular"
+    }))
   ]
   const displayedData =
     showMore || !isMobile ? combinedData : combinedData.slice(0, 3)
+
+  const saleCourses: CardSale[] = displayedData.filter(
+    (item): item is CardSale => item.type === "sale" && "accept" in item
+  )
+
+  const regularCourses: CardData[] = displayedData.filter(
+    (item): item is CardData => item.type === "regular" && "title" in item
+  )
 
   return (
     <section className="mb-[31px] w-full sm:mb-[51px] md:mb-[62px] md:px-5 xl:mb-[95px] xl:px-10 3xl:mb-[171px] 3xl:px-20 5xl:mb-[211px]">
@@ -36,12 +52,8 @@ const Courses = () => {
       <div>
         <div className="flex flex-col flex-wrap gap-[17px] sm:gap-[20px] md:flex-row md:flex-nowrap">
           <ul className="flex flex-wrap justify-center gap-[17px] sm:gap-[20px]">
-            <SaleCards
-              data={displayedData.filter((item) => item.type === "sale")}
-            />
-            <Cards
-              data={displayedData.filter((item) => item.type === "regular")}
-            />
+            {saleCourses && <SaleCards data={saleCourses} />}
+            <Cards data={regularCourses} />
           </ul>
           {!showMore && isMobile && (
             <button
