ae4761647e0f8d50b55079aba2e905cada9d42bf
Inna Shkilna <shkinai.1995@gmail.com>
2024-08-20 12:02:11 +0300

    Add adaptive to 1280/1440/1920px

--- src/components/main/courses/Title.tsx
+++ src/components/main/courses/Title.tsx
@@ -6,7 +6,7 @@ const Title = () => {
 
   return (
     <div className=" mb-[29px] flex  items-center gap-[16px] sm:mb-9 md:mb-10  xl:mb-[42px] ">
-      <Star className="hidden h-[30px] w-[30px] fill-[#AAAEDF] stroke-black md:block xl:h-[34px] xl:w-[34px] 3xl:h-[41px] 3xl:w-[41px]" />
+      <Star className="hidden h-[41px] w-[30px] fill-[#AAAEDF] stroke-black md:block xl:h-[41px] xl:w-[41px] 3xl:h-[41px] 3xl:w-[41px]" />
       <h2 className="text-[32px] font-semibold uppercase leading-[1.5] tracking-[0px] text-[#eaeaeb] sm:text-[36px] md:text-[48px] xl:text-[80px] 3xl:text-[100px]">
         {" "}
         {t("courses_section.title")}
