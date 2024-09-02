05a991e396d3af57eed10d0748eca9466df7806b
SvitlanaFediai <svitlana160393@gmail.com>
2024-09-02 20:20:02 +0300

    support page

---
+++ src/components/icons/accordion/ArrowRight.tsx
@@ -0,0 +1,21 @@
+const ArrowRightIcon = () => {
+  return (
+    <svg
+      width="19"
+      height="21"
+      viewBox="0 0 19 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.42871 4.5C10.1976 6.07283 11.1506 7.21297 12.1802 8.68457C12.9408 9.77172 12.9408 11.2283 12.1802 12.3154C11.1506 13.787 10.1976 14.9272 8.42871 16.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default ArrowRightIcon
